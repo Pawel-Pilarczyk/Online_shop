8761ee37e9c9fb17c522f4ec280d1a84084e88d6
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-03-14 19:04:50 +0100

    v0.01
    Prepeting new project files

---
+++ src/components/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Menu',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
