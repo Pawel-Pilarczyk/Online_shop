f04a4e59d246ec9d47a39c69084711cdbce5f29b
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-03-15 09:04:18 +0100

    v0.03

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -8,9 +8,10 @@
             <li><router-link to="/about">About us</router-link></li>
             <li><router-link to="/products">Products</router-link></li>
             <li><router-link to="/contact">Contact</router-link></li>
+            <li><router-link to="/cart">Cart</router-link></li>
         </ul>
 
-        <div id='burger' @click="menuToggle">
+        <div id='burger' @click="menuToggle" :class="burgerActive ? 'burger-active' : 'burger-unactive'">
           <div></div>
           <div></div>
           <div></div>
@@ -27,12 +28,14 @@ export default {
   data () {
     return {
       menuActive:false,
+      burgerActive: false
     }
   },
 
   methods:{
     menuToggle(){
       this.menuActive = !this.menuActive;
+      this.burgerActive =!this.burgerActive;
     }
   }
 }
