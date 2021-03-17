3a5e2965cd3306e8e1565a4ca3394a10d7e906cd
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-03-17 22:03:31 +0100

    v0.05

--- src/components/Baner.vue
+++ src/components/Baner.vue
@@ -1,21 +1,44 @@
 <template>
 
     <div class="carousel-container">
-        <div class="carousel-slide">
-            <img src="../media/pics/baner1.jpg" alt="">
+        <div class="carousel-slide" :style="{transform: 'translate(-' + banerWidth + 'px)' }">
+            <img src="../media/pics/baner3.jpg" id='lastClone' alt="">
+            <img src="../media/pics/baner1.jpg" alt="" >
             <img src="../media/pics/baner2.jpg" alt="">
             <img src="../media/pics/baner3.jpg" alt="">
+            <img src="../media/pics/baner1.jpg" id='firstClone' alt="">
         </div>
+
+        <button id='prevBtn'>Prev</button>
+        <button id='nextBtn' @click="incrementCounter">Next</button>
+
     </div>
+
+
 
 </template>
 <script>
+
+
+//
 
 export default {
     name:'Baner',
     data() {
         return {
 
+            counter:1,
+
+            banerWidth : document.querySelector('.carousel-slide').getBoundingClientRect().width,
+
+        }
+    },
+    methods:{
+        incrementCounter(){
+            this.counter++
+        },
+        decrementCounter(){
+            this.counter--
         }
     },
 }
