9b15498a860fba8d1493f82ddc81ae5fbf6d0ef3
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-03-22 09:55:51 +0100

    v0.111
    update of Product Component

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,17 @@
 <template>
     <div id="product">
+        <img :src="product.mainPicUrl" alt="">
         <h1>{{product.name}}</h1>
+
+        <h5 :class="product.specialPrice ? 'special-price' : ''">{{product.price}} $</h5>
+        <h5 v-if='product.specialPrice'>{{product.price * 0.8}} $</h5>
+        <h5>Dimentions {{ product.dimentions }}</h5>
+        <h5>Stock: {{product.stock}}</h5>
+
+        <button>Add to the cart</button>
+
+
+        <p>{{product.description}}</p>
     </div>
 </template>
 
@@ -9,7 +20,7 @@
         name:'Product',
         data() {
             return {
-              product: this.$store.state.products[this.$route.params.id],
+                product: this.$store.state.products[this.$route.params.id],
             }
         },
         methods:{
