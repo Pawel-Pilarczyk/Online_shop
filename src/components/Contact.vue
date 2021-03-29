604a203ba11ac34b63f38a82c3930c0048517006
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-03-29 09:39:27 +0200

    v0.132

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,17 +1,21 @@
 <template>
     <div id="contact">
         <div id="contactBaner">
-            <h3>Contact One of our Specialist</h3>
+            <h3>Contact One of our Specialists</h3>
             <div class="layer"></div>
         </div>
         <div id="contactOptions">
 
             <div id='phoneContact'>
                 <h1>Phone</h1>
+                <p>Interested in one of our Sofas? Just Pick up the phone and chat to our sales team</p>
+                <a href="tel:xxxxxxxx">1xxxxxxxxxxx</a>
             </div>
 
             <div id='chatContact'>
                 <h1>Chat</h1>
+                <p>Sometimes you need a little help from our friends or just chat to our Support. Don't worry - we are here for you</p>
+                <button>Open Chat</button>
             </div>
 
         </div>
