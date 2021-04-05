0c37ae7a86be353fb3fbd74c09dd99c8f8b2027d
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2021-04-05 16:23:14 +0200

    v0.212
    styles update

--- src/components/ProductEdit.vue
+++ src/components/ProductEdit.vue
@@ -1,21 +1,51 @@
 <template>
 
     <div id="productEdit">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+
+        <h1>{{productName}}</h1>
+
         <form>
-            <input type="text" name="productName" v-model="productName" :placeholder="productName">
-            <input type="number" :placeholder="stock" v-model="stock">
-            <input type="text" :placeholder="dimensions" v-model="dimensions">
-            <textarea type="text" :placeholder="description" v-model="description"></textarea>
-            <input type="checkbox" name="recommended"  v-model="recommended">
-            <input type="checkbox" name="specialPrice"  v-model="specialPrice">
+
+            <section>
+                <label for="productName">Product Name:</label>
+                <input type="text" name="productName" v-model="productName" :placeholder="productName">
+            </section>
+            
+            <section>
+                <label for="productPrice">Product Price:</label>
+                <input type="text" name="productPrice" v-model="productPrice" :placeholder="productPrice">
+            </section>
+
+            <section>
+                <label for="stockNumber">Stock:</label>
+                <input type="number" name='stockNumber' :placeholder="stock" v-model="stock">
+            </section>
+
+            <section>
+                <label for="dimensions">Dimentions(pattern aa x bb x cc)</label>
+                <input type="text" name='dimensions' :placeholder="dimensions" v-model="dimensions">
+            </section>
+
+            <section>
+                <label for="description">Description</label>
+                <textarea name='description' :placeholder="description" v-model="description"></textarea>
+            </section>
+
+            <section>
+                <label for="recommended">Recommended:</label>
+                <input type="checkbox" name="recommended"  v-model="recommended">
+            </section>
+
+            <section>
+                <label for="specialPrice">Special Price:</label>
+                <input type="checkbox" name="specialPrice"  v-model="specialPrice">
+            </section>
+            
+            <input type="button" value="Update Product">
+            <input type="button" value="Remove Product">
+
         </form>
+
     </div>
 
 </template>
@@ -26,6 +56,7 @@
             return {
                 product: this.$store.state.products[this.$route.params.id],
                 productName: "",
+                productPrice: NaN,
                 stock: NaN,
                 dimensions: '',
                 description:'',
@@ -36,6 +67,7 @@
         mounted: function () {
             this.$nextTick(function () {
                 this.productName = this.product.name;
+                this.productPrice = this.product.price;
                 this.stock = this.product.stock;
                 this.dimensions = this.product.dimentions;
                 this.description = this.product.description;
